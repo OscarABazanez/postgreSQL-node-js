80d587282933fbad74cf8c4d996ee2e9f413952e
Oscar Bazañez <oscar-bazanez@hotmail.com>
2021-10-13 12:03:11 -0500

    Relacion de Uno a Mucho, se crearon las tablas product y category

---
+++ db/migrations/20211013153420-productos.js
@@ -0,0 +1,15 @@
+'use strict';
+const { PRODUCT_TABLE, ProductSchema } = require('./../models/producto.model')
+const { CATEGORY_TABLE, CategorySchema } = require('./../models/category.model')
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(PRODUCT_TABLE);
+  }
+};
--- db/models/index.js
+++ db/models/index.js
@@ -1,14 +1,20 @@
 const{ User, UserSchema } = require('./user.model');
 const{ Customer, CustomerSchema } = require('./customer.model');
+const{ Product, ProductSchema } = require('./producto.model');
+const{ Category, CategorySchema } = require('./category.model');
 
 // Aqui deberan estar todos los modelos
 function setUpModels(sequelize){
-    User.init(UserSchema, User.config(sequelize))
-    Customer.init(CustomerSchema, Customer.config(sequelize))
+    User.init(UserSchema, User.config(sequelize));
+    Customer.init(CustomerSchema, Customer.config(sequelize));
+    Product.init(ProductSchema, Product.config(sequelize));
+    Category.init(CategorySchema, Category.config(sequelize));
 
     // Indicamos que tablas tiene una relacion
-    User.associate(sequelize.models)
-    Customer.associate(sequelize.models)
+    User.associate(sequelize.models);
+    Customer.associate(sequelize.models);
+    Category.associate(sequelize.models);
+    Product.associate(sequelize.models);
 }
 
 module.exports = setUpModels;
