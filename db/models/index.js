a7be2a33efa824742a724c446a9ec574dc4c009f
Oscar Bazañez <oscar-bazanez@hotmail.com>
2021-10-14 12:08:34 -0500

    Orden de compra, Relacion de Muchos a Muchos

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const{ User, UserSchema } = require('./user.model');
 const{ Customer, CustomerSchema } = require('./customer.model');
-const{ Product, ProductSchema } = require('./producto.model');
+const{ Product, ProductSchema } = require('./product.model');
 const{ Category, CategorySchema } = require('./category.model');
+const{ Order, OrderSchema } = require('./order.model');
+const{ OrderProduct, OrderProductSchema } = require('./order-product.models');
 
 // Aqui deberan estar todos los modelos
 function setUpModels(sequelize){
@@ -9,12 +11,15 @@ function setUpModels(sequelize){
     Customer.init(CustomerSchema, Customer.config(sequelize));
     Product.init(ProductSchema, Product.config(sequelize));
     Category.init(CategorySchema, Category.config(sequelize));
+    Order.init(OrderSchema, Order.config(sequelize));
+    OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
     // Indicamos que tablas tiene una relacion
     User.associate(sequelize.models);
     Customer.associate(sequelize.models);
     Category.associate(sequelize.models);
     Product.associate(sequelize.models);
+    Order.associate(sequelize.models);
 }
 
 module.exports = setUpModels;
