eed903382d19f56d5ade43155104769f90d1622c
Oscar Bazañez <oscar-bazanez@hotmail.com>
2021-10-15 12:44:30 -0500

    Fix migration

--- db/migrations/20211011171920-create-user.js
+++ db/migrations/20211011171920-create-user.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { USER_TABLE, UserSchema } = require('./../models/user.model')
+const { USER_TABLE } = require('./../models/user.model')
+const { DataTypes } = require('sequelize')
+
 // En este archivo se debe agregar todos los schema que tenemos
 
 module.exports = {
@@ -11,10 +13,32 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add reverting commands here.
      *
--- db/migrations/20211012151948-add-role.js
+++ db/migrations/20211012151948-add-role.js
@@ -1,5 +1,6 @@
 'use strict';
-const { USER_TABLE, UserSchema } = require('./../models/user.model')
+const { USER_TABLE } = require('./../models/user.model')
+const { DataTypes } = require('sequelize')
 
 module.exports = {
   up: async (queryInterface) => {
@@ -9,7 +10,11 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    await queryInterface.addColumn(USER_TABLE, 'role', {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'customer'
+    });
   },
 
   down: async (queryInterface) => {
@@ -19,6 +24,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-     await queryInterface.removeColumn(USER_TABLE, 'role');
+    await queryInterface.removeColumn(USER_TABLE, 'role');
   }
 };
